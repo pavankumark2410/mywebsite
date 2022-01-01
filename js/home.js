f53f5bf17722c902dd328ab49a90d8d8dd0d4963
pavankumark2410 <pavan.kumar@mca.christuniversity.in>
2022-01-01 13:11:38 +0530

    my commit

--- js/home.js
+++ js/home.js
@@ -3,18 +3,38 @@ $(document).ready(function() {
         $("#stage1").show();
         $("#stage2").hide();
         $("#stage3").hide();
+         $("#stage4").hide();
+          $("#stage5").hide();
     });
  
      $("#s2").click(function() {
         $("#stage1").hide();
         $("#stage2").show();
         $("#stage3").hide();
+         $("#stage4").hide();
+          $("#stage5").hide();
     });
 
       $("#s3").click(function() {
         $("#stage1").hide();
         $("#stage2").hide();
         $("#stage3").show();
+         $("#stage4").hide();
+          $("#stage5").hide();
+    });
+      $("#s4").click(function() {
+        $("#stage1").hide();
+        $("#stage2").hide();
+        $("#stage3").hide();
+         $("#stage4").show();
+          $("#stage5").hide();
+    });
+      $("#s5").click(function() {
+        $("#stage1").hide();
+        $("#stage2").hide();
+        $("#stage3").hide();
+         $("#stage4").hide();
+          $("#stage5").show();
     });
       $("#t1").click(function() {
         $("#type1").show();
@@ -25,3 +45,34 @@ $(document).ready(function() {
         $("#type1").hide();
     });
 });
+$(function() {
+    var target = $('.bottom-content');
+    if (target.length) {
+        $('html,body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+        return false;
+    }
+});
+$(function() {
+    $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 200);
+            return false;
+        }
+    });
+   $(document).ready(function() {
+   $('.main').hide();
+});
+    $(function() {
+  $(".main").delay(4000).fadeIn(1000);
+  setTimeout(() => {  
+  $('.loading-wrapper').fadeOut('fast');  
+}, 4000);
+});
+});
+
